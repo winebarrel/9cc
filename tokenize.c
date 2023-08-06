d5f620da3eed06fe18ff6bab13ae58cb9fb37ad1
winebarrel <sugawara@winebarrel.jp>
2023-08-06 16:14:47 +0900

    lvar

--- tokenize.c
+++ tokenize.c
@@ -35,6 +35,15 @@ bool consume(char *op) {
   return true;
 }
 
+// Consumes the current token if it is an identifier.
+Token *consume_ident() {
+  if (token->kind != TK_IDENT)
+    return NULL;
+  Token *t = token;
+  token = token->next;
+  return t;
+}
+
 // Ensure that the current token is `op`.
 void expect(char *op) {
   if (token->kind != TK_RESERVED || strlen(op) != token->len ||
@@ -70,9 +79,7 @@ bool is_alpha(char c) {
   return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_';
 }
 
-bool is_alnum(char c) {
-  return is_alpha(c) || ('0' <= c && c <= '9');
-}
+bool is_alnum(char c) { return is_alpha(c) || ('0' <= c && c <= '9'); }
 
 // Tokenize `user_input` and returns new tokens.
 Token *tokenize() {
@@ -104,8 +111,14 @@ Token *tokenize() {
     }
 
     // Single-letter punctuator
-    if (strchr("+-*/()<>;", *p)) {
+    if (strchr("+-*/()<>;=", *p)) {
       cur = new_token(TK_RESERVED, cur, p++, 1);
+      continue;
+    }
+
+    // Identifier
+    if ('a' <= *p && *p <= 'z') {
+      cur = new_token(TK_IDENT, cur, p++, 1);
       continue;
     }
 
